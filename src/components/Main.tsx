c5bb52933a241bd48276e8fd296af97bef422b72
waltbcc <waltbcc@gmail.com>
2026-01-09 10:57:06 -0300

    god bless walt

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,37 +1,63 @@
-import NestaEdicao from './NestaEdicao.tsx'
-import Textos from './Textos.tsx'
-import CapaJornais from './CapaJornais.tsx'
-
-interface CapaItem {
-  codimagem: number
-  caminho: string
-  relevancia: number
-}
+import type { ReactElement } from "react";
 
 interface AuditoriaItem {
-  [key: string]: any
+  titulo_1?: string
+  texto_1?: string
+  titulo_2?: string
+  texto_2?: string
+  titulo_3?: string
+  texto_3?: string
+  titulo_4?: string
+  texto_4?: string
+  titulo_5?: string
+  texto_5?: string
+  [key: string]: any // Para permitir acesso dinâmico às propriedades
 }
 
-interface MainProps {
+interface TextosProps {
   auditoria: AuditoriaItem[] | null
-  capas: CapaItem[] | null
 }
 
-function Main({ auditoria, capas }: MainProps) {
+function Textos({ auditoria }: TextosProps) {
+    // Dentro do seu componente:
+    let blocks: ReactElement[] = [];
+
+    if (auditoria && auditoria.length > 0) {
+        const item = auditoria[0]; // Pegando o primeiro item enviado pelo backend
+
+        for (let count = 1; count <= 5; count++) {
+            const titulo = item[`titulo_${count}`];
+            const texto = item[`texto_${count}`];
+
+            if (titulo && texto) {
+                blocks.push(
+                    <div key={count} className="flex flex-col w-full gap-2">
+                        <h3
+                            className="font-bold text-blue-900 text-lg lg:text-xl tracking-normal lg:tracking-wide mb-2"
+                            dangerouslySetInnerHTML={{ __html: titulo }}
+                        />
+
+                        <div className="flex flex-col gap-4">
+                            <p
+                                className="font-normal tracking-wide text-(--azul-paragrafo) text-sm md:text-base"
+                                dangerouslySetInnerHTML={{ __html: texto }}
+                            />
+                        </div>
+                    </div>
+                );
+            }
+        }
+    }
+
 
   return (
-    <main className="p-4">
-      <div className="mx-auto max-w-7xl flex gap-10 flex-col md:flex-row">
-        <section className="max-w-sm w-full h-full flex flex-col gap-10">
-          <NestaEdicao auditoria={auditoria} />
-          <CapaJornais capas={capas} />
-        </section>
-
-        <Textos auditoria={auditoria} />
-
+    <section className="w-full mx-auto">
+      <div className="flex flex-col gap-4 lg:gap-5">
+        {/* Renderiza automaticamente todos os blocos */}
+        {blocks}
       </div>
-    </main>
+    </section>
   )
 }
 
-export default Main;
+export default Textos;
